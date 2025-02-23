fa844198c0675c5eebc313ccdaf7171304fcaa55
sony <phenombox@gmail.com>
2025-02-23 23:01:24 +0800

    init

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/contact">Contact</NuxtLink>
+        <NuxtPage />
+    </div>
+</template>
+
+<script>
+definePageMeta({
+  layout: 'custom'
+})
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/contact">Contact</NuxtLink>
+        <NuxtPage />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
